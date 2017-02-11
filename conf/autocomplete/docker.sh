209e80b33ed4e1ce7254c802032e01b7c210cc50
vitorarins <vitorarins@gmail.com>
2017-02-11 17:11:40 -0200

    Added new docker alias; Refactor organization of completes.

---
+++ conf/autocomplete/docker.sh
@@ -0,0 +1,30 @@
+# docker auto complete
+
+# The `parts` argument hold the line splited by space.
+fn docker_complete(parts, line, pos) {
+	ret = ()
+
+	partsz <= len($parts)
+	
+	if $partsz != "1" {
+		# only autocomplete the subcommand
+		return $ret
+	}
+
+	choice <= (
+		echo "attach build commit cp create diff events exec export history images import info inspect kill load login logout logs network node pause port ps pull push rename restart rm rmi run save search service start stats stop swarm tag top unpause update version volume wait" |
+		tr " " "\n" |
+		-fzf --header "Docker subcommands: "
+						--reverse
+						 |
+		tr -d "\n"
+	)
+
+	if $status != "0" {
+		return $ret
+	}
+
+	ret = ($choice+" " "0")
+
+	return $ret
+}
