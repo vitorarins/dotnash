2a8d954a22e4d4761a2467ca0371d2cf5f9f780b
vitorarins <vitorarins@gmail.com>
2017-03-22 17:33:07 -0300

    Added autocomplete for Makefiles and ssh config.

--- conf/autocomplete/docker.sh
+++ conf/autocomplete/docker.sh
@@ -2,14 +2,10 @@
 
 # The `parts` argument hold the line splited by space.
 fn docker_complete(parts, line, pos) {
+
 	ret = ()
 
 	partsz <= len($parts)
-	
-	if $partsz != "1" {
-		# only autocomplete the subcommand
-		return $ret
-	}
 
 	choice <= (
 		echo "attach build commit cp create diff events exec export history images import info inspect kill load login logout logs network node pause port ps pull push rename restart rm rmi run save search service start stats stop swarm tag top unpause update version volume wait" |
