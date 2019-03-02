cbacfdc1519eecf1239ffd0ca12e386c8780eb6e
Vitor Arins <vitorarins@gmail.com>
2019-03-02 23:01:19 +0100

    Update to work with var.

--- conf/autocomplete/docker.sh
+++ conf/autocomplete/docker.sh
@@ -2,12 +2,10 @@
 
 # The `parts` argument hold the line splited by space.
 fn docker_complete(parts, line, pos) {
+	var ret = ()
 
-	ret = ()
-
-	partsz <= len($parts)
-
-	choice <= (
+	var partsz <= len($parts)
+	var choice <= (
 		echo "attach build commit cp create diff events exec export history images import info inspect kill load login logout logs network node pause port ps pull push rename restart rm rmi run save search service start stats stop swarm tag top unpause update version volume wait" |
 		tr " " "\n" |
 		-fzf --header "Docker subcommands: "
